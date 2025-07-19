dcf8b3788511b82de9a2509fdfe678a8d7616ce4
Ammar Khan <ammarkhanpathan421@gmail.com>
2025-07-19 11:03:06 +0530

    Update resume_checker_.py

--- resume_checker_.py
+++ resume_checker_.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Xl8yQEmUJmZ0ZovLyN9HVXdIK2yr7Q_u
 """
 
-!pip install gradio pdf2image pytesseract nltk matplotlib PyMuPDF
-!apt-get install -y poppler-utils
 
 #Import Libraries and Setup
 
